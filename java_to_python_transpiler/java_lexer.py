7c9dcdc938aa9140abe7666e3db40907471d34da
Hunter G <hunterg@pop-os.localdomain>
2023-08-21 12:44:26 -0500

    Created a number of token classes that can be used inside of the Lexer

--- java_to_python_transpiler/java_lexer.py
+++ java_to_python_transpiler/java_lexer.py
@@ -1,3 +1,12 @@
 """
-
+This module's purpose is to tokenize user input (some Java source code string)
 """
+
+
+class Lexer:
+    """
+    This class's purpose the same as this modules
+    """
+
+    def __init__(self, java_source: str):
+        self.java_source = java_source
