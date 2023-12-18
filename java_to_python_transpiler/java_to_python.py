6ee063e997bb60ab3c0a4fef20ac94731e8ce0bd
Hunter G <hunterg@pop-os.localdomain>
2023-12-17 20:58:15 -0600

    Rewrote NodeResult into two classes: NodeSuccess and NodeFailure. Used these classes in code and cleaned up A LOT

--- java_to_python_transpiler/java_to_python.py
+++ java_to_python_transpiler/java_to_python.py
@@ -4,10 +4,10 @@ from __future__ import annotations
 This module's purpose is to tokenize user input (some Java source code string)
 """
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import re
 from enum import Enum
-from typing import List, Optional, Union, Tuple, AnyStr
+from typing import List, Optional, Union, Tuple
 
 
 TokenType = str
@@ -107,7 +107,7 @@ class LexerFailure:
     error_message is the message to be printed to the user
     """
 
-    error_message: str = ""
+    error_message: str
 
 
 def scan_and_tokenize_input(user_input: str) -> Union[List[Token], LexerFailure]:
@@ -242,28 +242,27 @@ class ParserResult:
 
 
 @dataclass
-class NodeResult:
+class NodeSuccess:
     """
-    This class defines the output of a node.
+    Represents a successful node result.
 
-    Either ('tokens' and 'node') or 'error_message' must have a value; they
-    cannot all be empty.
-    
-    Specifically, if ('tokens' is an empty list) or (node is None), it
-    indicates an error, and 'error_message' must be a non-empty string.
-    
-    Conversely, if ('tokens' is not an empty list) and (node is not None),
-    'error_message' must be an empty string.
+    `tokens` is the remaining list of Tokens 
+    `node` is the output to be added to the AST
     """
 
-    was_successful: bool
-    tokens: Optional[List[Token]] = field(default_factory=list)
-    node: Union[ExpressionNode, TermNode, FactorNode, None] = None
-    error_message: str = ""
+    tokens: List[Token]
+    node: Union[ExpressionNode, TermNode, FactorNode]
 
 
-# Type alias that is used for ExpressionNode and TermNode
-ArithmeticOperatorMember = str
+@dataclass
+class NodeFailure:
+    """
+    Represents an error the occurs in a node.
+
+    `error_message` is the message to be printed for the user.
+    """
+
+    error_message: str
 
 
 @dataclass
@@ -288,7 +287,7 @@ class ExpressionNode:
     """
 
     single_term_node: TermNode
-    operator: Optional[ArithmeticOperatorMember] = None
+    operator: Optional[ArithmeticOperator] = None
     additional_expression_node: Optional[ExpressionNode] = None
 
 
@@ -311,7 +310,7 @@ class TermNode:
     """    
 
     first_factor_node: FactorNode
-    operator: Optional[ArithmeticOperatorMember] = None
+    operator: Optional[ArithmeticOperator] = None
     second_factor_node: Optional[FactorNode] = None
 
 
@@ -384,163 +383,84 @@ class ArgumentList:
     additional_argument_list: Optional[ArgumentList] = None
 
 
+NodeResult = Union[NodeSuccess, NodeFailure]
+
+
 def parse_list_of_tokens(tokens: List[Token]) -> ParserResult:
     """ This functions purpose is to be the entrypoint for the parser """
 
 
-    class ParserErrorReason:
-        """
-        Enumeration class representing reasons for parser errors.
-
-        Enum Members:
-        
-        - UNEXPECTED_TOKEN_TYPE: Indicates an unexpected token type during
-        parsing. Usage: UNEXPECTED_TOKEN_TYPE.format(token_type)
-        
-        - VALUE_IS_NULL: Indicates finding a null value during parsing. Usage:
-        VALUE_IS_NULL.format(value_name)
-        
-        - UNEXPECTED_TYPE: Indicates an unexpected node type during parsing.
-        Usage: UNEXPECTED_TYPE.format(node_type)
-        """
-        
-        UNEXPECTED_TOKEN_TYPE = "Unexpected Token Type, {0}"
-        VALUE_IS_NULL = "Found A Null Value; {0} is Null"
-        UNEXPECTED_TYPE = "Unexpected Node of Type, {0}"
-
-
-    def report_error(unexpected_token_type: Optional[TokenType] = None,
-                     unexpected_null: Optional[AnyStr] = None,
-                     unexpected_type: Optional[AnyStr] = None) -> NodeResult:
+    def report_error(unexpected_token_type: TokenType) -> NodeFailure:
         """
         This function's purpose is to report an error found in the parser
         """
 
-        error_message = ""
+        ERROR_MESSAGE: str = "Unexpected token type, {0}"
+        error_message: str = ERROR_MESSAGE.format(unexpected_token_type)
 
-        if unexpected_token_type is not None:
-            error_message: str = ParserErrorReason.UNEXPECTED_TOKEN_TYPE.format(
-                    unexpected_token_type)
-        
-        if unexpected_null is not None:
-            error_message: str = ParserErrorReason.VALUE_IS_NULL.format(unexpected_null)
-
-        if unexpected_type is not None:
-            error_message: str = ParserErrorReason.UNEXPECTED_TYPE.format(unexpected_type)
-
-        node_result = NodeResult(False, error_message=error_message)
-        return node_result
+        return NodeFailure(error_message)
 
 
     def parse_tokens_for_expression(tokens: List[Token]) -> NodeResult:
         """
         Parses a list of tokens to construct an abstract syntax tree (AST) for
         a mathematical expression.
-
-        Args:
-        - tokens (List[Token]): List of tokens to be parsed.
-
-        Returns:
-        - NodeResult: A result object containing information about the success
-        of the parsing operation. If successful, the result includes the parsed
-        expression node; otherwise, it contains an error message.
         """
-        
+
         EXPRESSION_TOKEN_TYPES: Tuple[TokenType, TokenType] = (PLUS_TOKEN_TYPE,
                                                                MINUS_TOKEN_TYPE)
  
         term_node_result: NodeResult = parse_tokens_for_term(tokens)
 
-        if not term_node_result.was_successful:
+        if isinstance(term_node_result, NodeFailure):
             return term_node_result
 
-        if not isinstance(term_node_result.node, TermNode):
+        assert isinstance(term_node_result.node, TermNode)
+        expression_node = ExpressionNode(term_node_result.node)
 
-            type_of_term_node = type(term_node_result.node)
-            UNEXPECTED_TYPE = str(type_of_term_node)
-
-            unsuccessful_result: NodeResult = report_error(unexpected_type=UNEXPECTED_TYPE)
-            return unsuccessful_result
-
-        first_term_node: TermNode = term_node_result.node
-        expression_node = ExpressionNode(first_term_node)
-
-        node_result_for_simple_expression = NodeResult(True, term_node_result.tokens,
-                                                       expression_node)
-        
-        if node_result_for_simple_expression.tokens is None:
-            FOUND_NULL_VALUE = "Node Result for Simple Expression is None"            
-
-            unsuccessful_result: NodeResult = report_error(unexpected_null=FOUND_NULL_VALUE)
-            return unsuccessful_result
+        node_success_for_simple_expression: NodeSuccess = \
+                NodeSuccess(term_node_result.tokens, expression_node)
 
         # We don't pop the token off b/c there's a chance it's not a + or -
         current_token: Token = tokens[0]
 
         if current_token.token_type == END_OF_FILE_TOKEN_TYPE:
-            return node_result_for_simple_expression 
+            return node_success_for_simple_expression 
 
         if current_token.token_type not in EXPRESSION_TOKEN_TYPES:
-            return node_result_for_simple_expression 
+            return node_success_for_simple_expression 
 
         del tokens[0]
 
-        expression_node_operator: ArithmeticOperatorMember 
+        expression_node_operator: ArithmeticOperator 
 
         if current_token.token_type == PLUS_TOKEN_TYPE:
             expression_node_operator = ArithmeticOperator.PLUS
         else:
             expression_node_operator = ArithmeticOperator.MINUS
-       
-        if term_node_result.tokens is None:
-            FOUND_NULL_VALUE = "Term Node Result Tokens is None"
 
-            unsuccessful_result: NodeResult = report_error(unexpected_null=FOUND_NULL_VALUE)
-            return unsuccessful_result
-
-        tokens = term_node_result.tokens 
-
-        additional_expression_node_result: NodeResult = parse_tokens_for_expression(tokens)
+        additional_expression_node_result: NodeResult = \
+                parse_tokens_for_expression(term_node_result.tokens)
         
-        if not additional_expression_node_result.was_successful:
+        if isinstance(additional_expression_node_result, NodeFailure):
             return additional_expression_node_result
 
-        if additional_expression_node_result.tokens is None:
-            FOUND_NULL_VALUE = "Additional Expression Node Result is None"
+        assert isinstance(additional_expression_node_result.node, ExpressionNode)
 
-            unsuccessful_result: NodeResult = report_error(unexpected_null=FOUND_NULL_VALUE)
-            return unsuccessful_result
+        complex_expression_node = ExpressionNode(
+            expression_node.single_term_node,
+            expression_node_operator,
+            additional_expression_node_result.node
+        )
 
-        tokens = additional_expression_node_result.tokens
-
-        additional_expression_node = additional_expression_node_result.node
-
-        if not isinstance(additional_expression_node, ExpressionNode):
-            type_of_additional_expression_node = type(additional_expression_node)
-            UNEXPECTED_TYPE = str(type_of_additional_expression_node)
-
-            unsuccessful_result: NodeResult = report_error(unexpected_type=UNEXPECTED_TYPE)
-            return unsuccessful_result
-
-        complex_term_node = ExpressionNode(expression_node.single_term_node,
-                                           expression_node_operator,
-                                           additional_expression_node)
-
-        return NodeResult(True, tokens, complex_term_node)
+        return NodeSuccess(additional_expression_node_result.tokens,
+                           complex_expression_node)
  
 
     def parse_tokens_for_term(tokens: List[Token]) -> NodeResult:
         """
         Parses a list of tokens to construct an abstract syntax tree (AST) for
         a mathematical term.
-
-        Args:
-        - tokens (List[Token]): List of tokens to be parsed.
-
-        Returns:
-        - NodeResult: A result object containing information about the success
-        of the parsing operation. If successful, the result includes the parsed
-        term node; otherwise, it contains an error message.
         """
 
 
@@ -549,83 +469,56 @@ def parse_list_of_tokens(tokens: List[Token]) -> ParserResult:
 
         factor_node_result: NodeResult = parse_tokens_for_factor(tokens)
 
-        if not factor_node_result.was_successful:
+        if isinstance(factor_node_result, NodeFailure):
             return factor_node_result
 
-        if not isinstance(factor_node_result.node, FactorNode):
-            type_of_factor_node_result_node = type(factor_node_result.node)
-            UNEXPECTED_TYPE = str(type_of_factor_node_result_node)
+        assert isinstance(factor_node_result.node, FactorNode)
 
-            unsuccessful_result: NodeResult = report_error(unexpected_type=UNEXPECTED_TYPE)
-            return unsuccessful_result
+        term_node = TermNode(factor_node_result.node)
 
-        first_factor_node: FactorNode = factor_node_result.node
-        term_node = TermNode(first_factor_node)
-
-        node_result_for_simple_term = NodeResult(True, factor_node_result.tokens, term_node)
-
-        if node_result_for_simple_term.tokens is None:
-            FOUND_NULL_VALUE = "Node Result for Simple Term's Tokens"
-            unsuccessful_result: NodeResult = report_error(unexpected_null=FOUND_NULL_VALUE)
-            return unsuccessful_result
+        node_success_for_simple_term: NodeSuccess = \
+                NodeSuccess(factor_node_result.tokens, term_node)
 
         # We don't pop the token off b/c there's a chance it's not a * or /
         current_token = tokens[0]
 
         if current_token.token_type == END_OF_FILE_TOKEN_TYPE:
-            return node_result_for_simple_term
+            return node_success_for_simple_term 
 
         if current_token.token_type not in TERM_TOKEN_TYPES:
-            return node_result_for_simple_term 
+            return node_success_for_simple_term 
 
         del tokens[0]
 
-        term_node_operator: ArithmeticOperatorMember 
+        term_node_operator: ArithmeticOperator 
 
         if current_token.token_type == MULTIPLY_TOKEN_TYPE:
             term_node_operator = ArithmeticOperator.MULTIPLY
         else:
             term_node_operator = ArithmeticOperator.DIVIDE
 
-        if factor_node_result.tokens is not None:
-            tokens = factor_node_result.tokens
+        # this might break something but idk
+#        if factor_node_result.tokens is not None:
+#            tokens = factor_node_result.tokens
 
         second_factor_node_result: NodeResult = parse_tokens_for_factor(tokens)
-        
-        if not second_factor_node_result.was_successful:
+       
+        if isinstance(second_factor_node_result, NodeFailure):
             return second_factor_node_result
 
-        second_factor_node_for_complex_term_node = second_factor_node_result.node
+        assert isinstance(second_factor_node_result.node, FactorNode)
+ 
+        complex_term_node: TermNode = TermNode(term_node.first_factor_node,
+                                               term_node_operator,
+                                               second_factor_node_result.node)
 
-        if second_factor_node_result.tokens is None:
-            unsuccessful_result: NodeResult = report_error("Null Error")
-            return unsuccessful_result
-
-        tokens = second_factor_node_result.tokens
-
-        if not isinstance(second_factor_node_for_complex_term_node, FactorNode):
-            unsuccessful_result: NodeResult = report_error("nil")
-            return unsuccessful_result
-
-        complex_term_node = TermNode(term_node.first_factor_node,
-                                     term_node_operator,
-                                     second_factor_node_for_complex_term_node)
-
-        return NodeResult(True, tokens, complex_term_node)
-            
+        return NodeSuccess(second_factor_node_result.tokens, complex_term_node)
+ 
 
     def parse_tokens_for_factor(tokens: List[Token]) -> NodeResult:
         """
         Parses a list of tokens to construct an abstract syntax tree (AST) for
         a mathematical term.
-
-        Args:
-        - tokens (List[Token]): List of tokens to be parsed.
-
-        Returns:
-        - NodeResult: A result object containing information about the success
-        of the parsing operation. If successful, the result includes the parsed
-        term node; otherwise, it contains an error message.
         """
 
         FACTOR_TOKEN_TYPES: List[str] = [
@@ -638,22 +531,21 @@ def parse_list_of_tokens(tokens: List[Token]) -> ParserResult:
         if current_token.token_type not in FACTOR_TOKEN_TYPES:
             return report_error(current_token.token_type)
 
-        factor_node: FactorNode = FactorNode(
-                number_or_identifier=current_token.value)
-
-        return NodeResult(True, tokens, factor_node)
+        factor_node: FactorNode = FactorNode(number_or_identifier=current_token.value)
+        return NodeSuccess(tokens, factor_node)
 
 
     root_node_result: NodeResult = parse_tokens_for_expression(tokens)
 
-    if not root_node_result.was_successful:
+    if isinstance(root_node_result, NodeFailure):
         return ParserResult(False, error_message=root_node_result.error_message)
 
     if not isinstance(root_node_result.node, ExpressionNode):
 
-        unsuccessful_result: NodeResult = report_error(str(root_node_result.node))
-        return ParserResult(False, error_message=unsuccessful_result.error_message)
+        root_node_as_str: str = str(root_node_result.node)
+        node_failure: NodeFailure = report_error(root_node_as_str)
+        
+        return ParserResult(False, error_message=node_failure.error_message)
 
-    root_node: ExpressionNode = root_node_result.node
-    return ParserResult(True, root_node)
+    return ParserResult(True, root_node_result.node)
 
