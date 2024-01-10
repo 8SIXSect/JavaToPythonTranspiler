26e5c0c48d4273fc14d3cf6c2cbea913f3e49273
Hunter G <hunterg@pop-os.localdomain>
2024-01-09 22:34:09 -0600

    Moved main.py to the .gitignore in preparation for publishing the project

--- main.py
+++
@@ -1,8 +0,0 @@
-from java_to_python_transpiler.transpile_methods import parse_file_and_print_ast
-
-
-if __name__ == "__main__":
-    
-     FILE_NAME = "sample.java"
-     parse_file_and_print_ast(FILE_NAME)
-
